b1cfb6eedd10457a4605e34617439d9be83f4cd2
HarrisonEXE <hdavidmelton@gmail.com>
2022-04-22 12:18:14 -0400

    First Person Controller
    - Camera button to change view
    - Escape to leave
    - Tab to change cursor lock

--- Assets/Scripts/CamSwitch.cs
+++ Assets/Scripts/CamSwitch.cs
@@ -6,10 +6,53 @@ public class CamSwitch : MonoBehaviour
 {
     public GameObject mainCamera;
     public GameObject secondCamera;
+    public GameObject fpsMode;
+    public bool isToggled = false;
 
     public void SwitchView()
     {
-        mainCamera.SetActive(!mainCamera.active);
-        secondCamera.SetActive(!secondCamera.active);
+        if (mainCamera.active)
+        {
+            mainCamera.SetActive(false);
+            secondCamera.SetActive(true);
+        } 
+        else if (secondCamera.active)
+        {
+            secondCamera.SetActive(false);
+            fpsMode.SetActive(true);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            fpsMode.SetActive(false);
+            mainCamera.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void Update()
+    {
+        if (fpsMode.active) 
+        {
+            if (Input.GetButton("Cancel"))
+            {
+                fpsMode.SetActive(false);
+                mainCamera.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+            }
+            if (Input.GetButton("Toggle"))
+            {
+                if (!isToggled)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    isToggled = true;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    isToggled = false;
+                }
+            }
+        }
     }
 }
